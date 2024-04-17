4c0c228df549ea8428271a893ff5272b4320fe80
Sergei Komisarchuk <KomisarchukSV@hg.group>
2024-04-17 21:45:38 +0300

    add homework 3

---
+++ HW3/homework_group_by-188-f40326_KomisarchukSV.sql
@@ -0,0 +1,105 @@
+/*
+Домашнее задание по курсу MS SQL Server Developer в OTUS.
+Занятие "02 - Оператор SELECT и простые фильтры, GROUP BY, HAVING".
+
+Задания выполняются с использованием базы данных WideWorldImporters.
+
+Бэкап БД можно скачать отсюда:
+https://github.com/Microsoft/sql-server-samples/releases/tag/wide-world-importers-v1.0
+Нужен WideWorldImporters-Full.bak
+
+Описание WideWorldImporters от Microsoft:
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-what-is
+* https://docs.microsoft.com/ru-ru/sql/samples/wide-world-importers-oltp-database-catalog
+*/
+
+-- ---------------------------------------------------------------------------
+-- Задание - написать выборки для получения указанных ниже данных.
+-- ---------------------------------------------------------------------------
+
+USE WideWorldImporters
+
+/*
+1. Посчитать среднюю цену товара, общую сумму продажи по месяцам.
+Вывести:
+* Год продажи (например, 2015)
+* Месяц продажи (например, 4)
+* Средняя цена за месяц по всем товарам
+* Общая сумма продаж за месяц
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+SELECT YEAR(inv.InvoiceDate) as InvoiceYear
+	,MONTH(inv.InvoiceDate) as InvoiceMonth
+	,AVG(invl.UnitPrice) as AvgPrice
+	,SUM(invl.ExtendedPrice) as SumPrice
+FROM Sales.Invoices as inv
+INNER JOIN Sales.InvoiceLines as invl
+ON inv.InvoiceID = invl.InvoiceID
+GROUP BY YEAR(inv.InvoiceDate)
+	,MONTH(inv.InvoiceDate)
+ORDER BY InvoiceYear, InvoiceMonth
+
+/*
+2. Отобразить все месяцы, где общая сумма продаж превысила 4 600 000
+
+Вывести:
+* Год продажи (например, 2015)
+* Месяц продажи (например, 4)
+* Общая сумма продаж
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+SELECT YEAR(inv.InvoiceDate) as InvoiceYear
+	,MONTH(inv.InvoiceDate) as InvoiceMonth
+FROM Sales.Invoices as inv
+INNER JOIN Sales.InvoiceLines as invl
+ON inv.InvoiceID = invl.InvoiceID
+GROUP BY YEAR(inv.InvoiceDate)
+	,MONTH(inv.InvoiceDate)
+HAVING 	SUM(invl.ExtendedPrice) > 4600000
+ORDER BY InvoiceYear, InvoiceMonth
+
+/*
+3. Вывести сумму продаж, дату первой продажи
+и количество проданного по месяцам, по товарам,
+продажи которых менее 50 ед в месяц.
+Группировка должна быть по году,  месяцу, товару.
+
+Вывести:
+* Год продажи
+* Месяц продажи
+* Наименование товара
+* Сумма продаж
+* Дата первой продажи
+* Количество проданного
+
+Продажи смотреть в таблице Sales.Invoices и связанных таблицах.
+*/
+
+SELECT YEAR(inv.InvoiceDate) as InvoiceYear
+	,MONTH(inv.InvoiceDate) as InvoiceMonth
+	,invl.Description
+	,SUM(invl.ExtendedPrice)
+	,MIN(inv.InvoiceDate)
+	,SUM(invl.Quantity)
+FROM Sales.Invoices as inv
+INNER JOIN Sales.InvoiceLines as invl
+ON inv.InvoiceID = invl.InvoiceID
+GROUP BY YEAR(inv.InvoiceDate)
+	,MONTH(inv.InvoiceDate)
+	,invl.Description
+HAVING SUM(invl.Quantity) < 50
+ORDER BY InvoiceYear, InvoiceMonth
+
+SELECT * FROM Sales.InvoiceLines
+
+-- ---------------------------------------------------------------------------
+-- Опционально
+-- ---------------------------------------------------------------------------
+/*
+Написать запросы 2-3 так, чтобы если в каком-то месяце не было продаж,
+то этот месяц также отображался бы в результатах, но там были нули.
+*/
